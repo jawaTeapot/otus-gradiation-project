3d25bb4e33a909a85d5c5b8478238b8f55c3d92d
Alexandr Pilipenko <jawa_teapot@bk.ru>
2024-01-16 16:55:30 +0300

    add: recovery page

--- pages/app/password-recovery.vue
+++ pages/app/password-recovery.vue
@@ -1,16 +1,121 @@
 <template>
-  <div>
-    Recovery Password
+  <div class="grid grid-cols-1 gap-5 lg:grid-cols-[1fr_1fr] max-w-[1180px] mx-auto p-3">
+    <el-card>
+      <h1 class="text-2xl font-bold mb-5 w-[500px]">
+        {{ $t('recoveryPage.title') }}
+      </h1>
+      <el-form
+        ref="formRef"
+        :model="passwordRecovery"
+        :rules="formRules"
+        size="large"
+        label-position="top"
+        status-icon
+        @submit.prevent="submitForm"
+      >
+        <el-form-item :label="$t('labels.email')" prop="email">
+          <el-input v-model="passwordRecovery.email" />
+        </el-form-item>
+        <el-form-item :label="$t('labels.verification-code')" prop="code">
+          <auth-verification ref="auth" v-model="passwordRecovery.code" />
+        </el-form-item>
+        <el-form-item>
+          <el-button class="w-full" type="success" native-type="submit">
+            {{ $t('recoveryPage.btn') }}
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <div>
+      <el-card class="mb-5">
+        <div class="text-lg font-bold">
+          {{ $t('loginPage.not-registered-yet') }}
+        </div>
+        <el-link type="primary" :href="localePath({ path: '/app/registration' })">
+          {{ $t('loginPage.go-to-the-registration-page') }}
+        </el-link>
+      </el-card>
+      <el-card>
+        <div class="text-lg font-bold">
+          {{ $t('registrationPage.already-registered') }}
+        </div>
+        <el-link type="primary" :href="localePath({ path: '/app/login' })">
+          {{ $t('registrationPage.go-to-the-login-page') }}
+        </el-link>
+      </el-card>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+import { ref, reactive } from 'vue'
+import { ElNotification, ElMessage } from 'element-plus'
+import type { FormInstance, FormRules } from 'element-plus'
+
 definePageMeta({
   public: true,
   layout: 'auth'
 })
+
+const { t } = useI18n()
+const localePath = useLocalePath()
+const formRef = ref<FormInstance>()
+const auth = ref()
+const passwordRecovery = reactive({
+  email: '',
+  password: '',
+  code: ''
+})
+
+const formRules = reactive<FormRules>({
+  email: [
+    { required: true, message: t('errors.required-to-fill'), trigger: 'blur' },
+    {
+      type: 'email',
+      message: t('errors.invalid-email'),
+      trigger: ['blur', 'change']
+    }
+  ],
+  code: [
+    { required: true, message: t('errors.required-to-fill'), trigger: 'blur' },
+    {
+      validator: (rule: any, value: any, callback: any) => {
+        if ((auth.value?.randomNumber1 + auth.value?.randomNumber2) !== +value) {
+          callback(new Error(t('errors.invalid-number')))
+        }
+        callback()
+      },
+      trigger: 'blur'
+    }
+  ]
+})
+
+const submitForm = async () => {
+  if (!formRef.value) { return }
+  const runtimeConfig = useRuntimeConfig()
+  await formRef.value.validate(async (valid) => {
+    if (valid) {
+      try {
+        const { email } = passwordRecovery
+        await $fetch(`${runtimeConfig.public.apiBase}/api/v1/password/forgot?email=${email}`, { method: 'POST' })
+        ElNotification({
+          title: t('labels.notification-success'),
+          message: t('labels.notification-massage'),
+          type: 'success'
+        })
+      } catch (e) {
+        ElMessage({
+          grouping: true,
+          center: true,
+          message: t('errors.something'),
+          type: 'error'
+        })
+      }
+      return navigateTo(localePath({ path: '/app/recovery' }))
+    }
+  })
+}
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
 </style>
---
+++ pages/app/recovery.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="grid grid-cols-1 gap-5 lg:grid-cols-[1fr_1fr] max-w-[1180px] mx-auto p-3">
+    <el-card>
+      <h1 class="text-2xl font-bold">
+        Изменение пароля
+      </h1>
+      <p class="my-5">
+        На ваш Email был отправлен код восстановления пароля.
+      </p>
+      <el-form
+        ref="formRef"
+        :model="recoveryForm"
+        :rules="formRules"
+        size="large"
+        label-position="top"
+        status-icon
+        @submit.prevent="submitForm"
+      >
+        <el-form-item label="Код" prop="recoveryCode">
+          <el-input v-model="recoveryForm.code" />
+        </el-form-item>
+        <el-form-item :label="$t('labels.newPassword')" prop="password">
+          <el-input v-model="recoveryForm.password" type="password" show-password />
+        </el-form-item>
+        <el-form-item :label="$t('labels.examinationPassword')" prop="repeatPassword">
+          <el-input v-model="recoveryForm.repeatPassword" type="password" show-password />
+        </el-form-item>
+        <auth-verification ref="auth" v-model="recoveryForm.codeCheck" />
+        <el-form-item>
+          <el-button class="w-full" type="success" :loading="isLoading" native-type="submit">
+            Изменить
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <div>
+      <el-card class="mb-5">
+        <div class="text-lg font-bold">
+          {{ $t('loginPage.not-registered-yet') }}
+        </div>
+        <el-link type="primary" :href="localePath({ path: '/app/registration' })">
+          {{ $t('loginPage.go-to-the-registration-page') }}
+        </el-link>
+      </el-card>
+      <el-card>
+        <div class="text-lg font-bold">
+          {{ $t('registrationPage.already-registered') }}
+        </div>
+        <el-link type="primary" :href="localePath({ path: '/app/login' })">
+          {{ $t('registrationPage.go-to-the-login-page') }}
+        </el-link>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ElMessage } from 'element-plus'
+import type { FormInstance, FormRules } from 'element-plus'
+import { ref, reactive } from 'vue'
+
+definePageMeta({
+  public: true,
+  layout: 'auth'
+})
+
+const { t } = useI18n()
+const localePath = useLocalePath()
+const formRef = ref<FormInstance>()
+const auth = ref()
+const route = useRoute()
+const router = useRouter()
+const code = ref(route.query['recovery-code'] ? String(route.query['recovery-code']) : '')
+
+const recoveryForm = ref({
+  code: code.value,
+  password: '',
+  repeatPassword: '',
+  codeCheck: ''
+})
+
+const isLoading = ref(false)
+const formRules = reactive<FormRules>({
+  code: [
+    { required: true, message: t('errors.required-to-fill'), trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: t('errors.required-to-fill'), trigger: 'blur' },
+    { min: 6, max: 255, message: 'Пароль должен быть минимум 6 знаков, максимум 255', trigger: 'blur' }
+  ],
+  repeatPassword: [
+    { required: true, message: t('errors.required-to-fill'), trigger: 'blur' },
+    {
+      validator: (rule: any, value: string, callback: any) => {
+        if (value !== recoveryForm.value.password) {
+          callback(new Error('Не верный пароль'))
+        }
+        callback()
+      },
+      trigger: 'blur'
+    }
+  ],
+  codeCheck: [
+    { required: true, message: t('errors.required-to-fill'), trigger: 'blur' },
+    {
+      validator: (rule: any, value: any, callback: any) => {
+        if ((auth.value?.randomNumber1 + auth.value?.randomNumber2) !== +value) {
+          callback(new Error(t('errors.invalid-number')))
+        }
+        callback()
+      },
+      trigger: 'blur'
+    }
+  ]
+})
+
+const submitForm = async () => {
+  if (!formRef.value) { return }
+  const runtimeConfig = useRuntimeConfig()
+  isLoading.value = true
+  try {
+    await formRef.value.validate()
+    const { code, password } = recoveryForm.value
+    await $fetch(`${runtimeConfig.public.apiBase}/api/v1/password/recovery`, {
+      method: 'POST',
+      body: {
+        code,
+        password
+      }
+    })
+    await router.replace({ query: undefined })
+    return navigateTo(localePath({ path: '/app/login' }))
+  } catch (e) {
+    ElMessage({
+      grouping: true,
+      center: true,
+      message: t('errors.something'),
+      type: 'error'
+    })
+  } finally {
+    isLoading.value = false
+  }
+}
+</script>
+
+  <style scoped lang="scss">
+  </style>
