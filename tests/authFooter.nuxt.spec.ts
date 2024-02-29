38190acbb84b9f41b8b0c0669ead192477bd8087
Alexandr Pilipenko <jawa_teapot@bk.ru>
2024-02-29 19:10:43 +0300

    add test appFooter

---
+++ tests/authFooter.nuxt.spec.ts
@@ -0,0 +1,33 @@
+import { describe, beforeEach, expect, it } from 'vitest'
+import { setActivePinia, createPinia } from 'pinia'
+import { mount } from '@vue/test-utils'
+import component from '../components/auth/authFooter.vue'
+
+describe('authFooter component', () => {
+  function buildWrapper (path = '/app') {
+    const wrapper = mount(component, {
+      global: {
+        stubs: {
+          NuxtLink: true // Стабируем NuxtLink
+        },
+        mocks: {
+          $route: {
+            path
+          },
+          $t: (msg: string) => msg
+        }
+      }
+    })
+    return wrapper
+  }
+
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  it('mounts without errors', () => {
+    const wrapper = buildWrapper('/')
+    expect(wrapper.exists()).toBe(true)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
